3639ae51795aef97bbed2616ba8383115c0052d1
evelyn-badescu <evelyn.badescu.17@gmail.com>
2023-04-20 14:54:03 +0300

    added some code for facebook login

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is an experiment");
         System.out.println("Hello world!!!!!");
+        System.out.println("Working on login with facebook");
     }
 }
