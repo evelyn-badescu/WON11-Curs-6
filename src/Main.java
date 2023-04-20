7fadc837d98525fa5ae2de574b49a93959e5992d
evelyn-badescu <evelyn.badescu.17@gmail.com>
2023-04-20 14:31:43 +0300

    first experiment commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is an experiment");
         System.out.println("Hello world!!!!!");
+        Lamp lamp1 = new Lamp();
+        Lamp lamp2 = new Lamp();
+
+        lamp1.turnOn();
+        lamp2.turnOn();
+
+        Lamp customLamp = new Lamp("green", 10);
+        customLamp.turnOn();
+        customLamp.increaseIntensity();
+        customLamp.turnOff();
+        customLamp.turnOn();
+
+        Random random = new Random();
+        random.nextInt();
+
     }
 }
