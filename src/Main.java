c39fd5ed70319ed613df21fb056b237efad5e402
evelyn-badescu <evelyn.badescu.17@gmail.com>
2023-04-20 14:55:51 +0300

    added final changes for facebook login

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("This is an experiment");
         System.out.println("Hello world!!!!!");
         System.out.println("Working on login with facebook");
+        System.out.println("Finished login with facebook");
     }
 }
